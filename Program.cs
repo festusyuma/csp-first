97b3129a5c6d952c01b3dd1a82ec3737b717bffd
festus <festusyuma@gmail.com>
2022-01-25 15:33:05 +0100

    updates

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
-﻿namespace csp_first
+﻿using System;
+
+namespace csp_first
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            const string name = "Festus";
+            const int age = 23;
+            const double wallet = 100.5567;
+            const bool goodBoy = true;
+            
+            Console.WriteLine("Hello " + name + "you are now " + age + " years old");
+            Console.ReadLine();
         }
     }
 }
