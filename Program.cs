1b565e82a884a96d77ce8ef2dc305fa6d5010ecf
festusyuma <festusyuma@gmail.com>
2022-01-25 22:13:05 +0100

    random nuber generator

--- Program.cs
+++ Program.cs
@@ -6,6 +6,59 @@ namespace csp_first
     {
         public static void Main(string[] args)
         {
+            ShowAppInfo();
+            
+            // Get player name
+            Console.Write("Enter your username: ");
+            var username = Console.ReadLine();
+            Console.WriteLine("************************");
+
+            Console.WriteLine("Welcome {0}, let's play a game...", username);
+
+            while (true)
+            {
+                var randomNumber = GenerateRandomNumber();
+                int? guess = 0;
+            
+                // Ask user for input
+                do
+                {
+                    Console.WriteLine("Guess a number between 1 and 10");
+                    guess = GetUserGuess();
+                    if (guess == null) continue;
+
+                    // Check if guess correct
+                    if (guess == randomNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Correct guess, you must be really smart");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Wrong!!!!, please try again");
+                    }
+                
+                    // Reset color
+                    Console.ResetColor();
+                
+                } while (guess != randomNumber);
+                
+                Console.WriteLine("Do you want to play again (enter y to continue)?");
+                var answer = Console.ReadLine()?.ToLower();
+
+                if (answer != "y")
+                {
+                    Console.WriteLine("Goodbye, thank you for playing");
+                    break;
+                }
+                
+                Console.WriteLine("");
+            }
+        }
+
+        private static void ShowAppInfo()
+        {
             const string appName = "Number Guesser";
             const string appVersion = "1.0.0";
             const string appAuthor = "Festus";
@@ -16,53 +69,28 @@ namespace csp_first
             
             // Reset text color
             Console.ResetColor();
-            
-            // Get player name
-            Console.Write("Enter your username: ");
-            var username = Console.ReadLine();
-            Console.WriteLine("************************");
+        }
 
-            Console.WriteLine("Welcome {0}, let's play a game...", username);
-            
-            // Generate random number
-            const int randomNumber = 7;
-            var guess = 0;
-            
-            // Ask user for input
-            do
+        static int? GetUserGuess()
+        {
+            try
             {
-                Console.WriteLine("Guess a number between 1 and 10");
-
-                try
-                {
-                    guess = Convert.ToInt32(Console.ReadLine());
-                }
-                catch (Exception e)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Enter a valid number");
-                    Console.ResetColor();
-                    continue;
-                }
-
-                // Check if guess correct
-                if (guess == randomNumber)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Correct guess, you must be really smart");
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Wrong!!!!, please try again");
-                }
-                
-                // Reset color
+                return Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Enter a valid number");
                 Console.ResetColor();
-                
-            } while (guess != randomNumber);
-            
-            Console.ReadLine();
+            }
+
+            return null;
+        }
+
+        static int GenerateRandomNumber()
+        {
+            var random = new Random();
+            return random.Next(1, 10);
         }
     }
 }
